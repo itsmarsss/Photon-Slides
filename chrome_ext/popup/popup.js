db2100262361e410a1344ae7386471d9be03a244
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-31 15:17:07 -0400

    Add default horizontal scrolling

--- chrome_ext/popup/popup.js
+++ chrome_ext/popup/popup.js
@@ -8,3 +8,18 @@ chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         console.log(response);
     });
 });
+
+Array.prototype.forEach.call(document.getElementsByClassName("scroll"), (element) => {
+    element.addEventListener("wheel", (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        element.scrollLeft += event.deltaY;
+    });
+});
+
+document.getElementById("cloud_new").addEventListener("click", () => {
+
+});
+document.getElementById("local_new").addEventListener("click", () => {
+
+});
