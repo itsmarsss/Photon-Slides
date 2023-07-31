bd25ece73f0297e1ae62bdf32f00f10d6afb9fb9
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-31 17:17:54 -0400

    Add import functionality

--- chrome_ext/popup/popup.js
+++ chrome_ext/popup/popup.js
@@ -14,48 +14,70 @@ const cloud_list = document.getElementById("cloud_list");
 const local_list = document.getElementById("local_list");
 
 const slide_source = document.getElementById("slide_source");
+const manual_textarea = document.getElementById("manual");
+const location_select = document.getElementById("location_select");
 
 document.getElementById("cloud_new").addEventListener("mouseup", () => {
     showSlideSource();
-
-    //newCloud();
 });
-
-function newCloud() {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, { "message": "query" }, function (response) {
-            cloud_list.innerHTML = `<div class="slide">
-            ${JSON.parse(response).name}
-            </div>` + cloud_list.innerHTML;
-
-            document.getElementById("cloud_new").addEventListener("mousedown", () => {
-                newCloud();
-            });
-        });
-    });
-}
 
 document.getElementById("local_new").addEventListener("click", () => {
-    //newLocal();
+    showSlideSource();
 });
 
-function newLocal() {
+document.getElementById("cancel_new").addEventListener("click", () => {
+    hideSlideSource();
+});
+
+document.getElementById("manual_button").addEventListener("click", () => {
+    if (location_select.value === "cloud") {
+        newCloud(manual_textarea.value);
+    } else {
+        newLocal(manual_textarea.value);
+    }
+
+    hideSlideSource();
+});
+
+document.getElementById("website_button").addEventListener("click", () => {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         var activeTab = tabs[0];
         chrome.tabs.sendMessage(activeTab.id, { "message": "query" }, function (response) {
-            local_list.innerHTML = `<div class="slide">${JSON.parse(response).name}</div>` + local_list.innerHTML;
+            if (location_select.value === "cloud") {
+                newCloud(response);
+            } else {
+                newLocal(response);
+            }
 
-            document.getElementById("local_new").addEventListener("click", () => {
-                newLocal();
-            });
+            hideSlideSource();
         });
     });
-}
+});
 
 function showSlideSource() {
     slide_source.style.opacity = "1";
     slide_source.style.transform = "scale(1)";
+}
+
+function hideSlideSource() {
+    slide_source.style.opacity = "0";
+    slide_source.style.transform = "scale(0)";
+}
+
+function newCloud(slides) {
+    cloud_list.innerHTML = `<div class="slide">${JSON.parse(slides).name} </div>` + cloud_list.innerHTML;
+
+    document.getElementById("cloud_new").addEventListener("mousedown", () => {
+        showSlideSource();
+    });
+}
+
+function newLocal(slides) {
+    local_list.innerHTML = `<div class="slide">${JSON.parse(slides).name}</div>` + local_list.innerHTML;
+
+    document.getElementById("local_new").addEventListener("click", () => {
+        showSlideSource();
+    });
 }
 
 async function setSlides() {
